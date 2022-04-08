8bff7a4ff53035bfeaa9fdd54273d1cd55b7e911
FireACNC <98172796+FireACNC@users.noreply.github.com>
2022-04-08 03:56:06 -0400

    Add files via upload

---
+++ mazeAlgorithm.py
@@ -0,0 +1,419 @@
+from cmu_cs3_graphics import *
+from cmu_graphics.utils import *
+import random, time, copy
+
+def onAppStart(app):
+    app.row,app.col = 25,25
+    app.gridSize = 20
+    app.width,app.height = 800,800
+    app.margin = 150
+    app.title = 'Initialized Maze'
+    initMaze(app)
+
+def initMaze(app):
+    app.maze = [[0]*app.row for col in range(app.col)]
+    app.maze[0][1] = app.maze[-1][-2] = 1
+
+    #taking even row/col as walls and odd ones as grid
+    app.grids = set()
+    for row in range(1,app.row,2):
+        for col in range(1,app.col,2):
+            app.grids.add((row,col))
+            app.maze[row][col] = 1
+            #grids are default empty
+    
+    app.walls = set()
+    #not including the edges of maze
+    for row in range(1,app.row-1):
+        for col in range(1,app.col-1):
+            if (row,col) not in app.grids and (row%2 != 0 or col%2 != 0): 
+                app.walls.add((row,col))
+
+
+################################################################################
+
+#All following methods are learned merely from descriptions in 
+#http://www.astrolog.org/labyrnth/algrithm.htm#perfect
+#and http://weblog.jamisbuck.org
+
+#some functions, such as nextGridIsValid, 
+#are shared between algorithms.
+
+################################################################################
+
+#Recursive backtracker
+def btGenerateMaze(app,visited,lastPos):
+    #if all grids (odd positions) visited, maze is complete.
+    if visited == app.grids: return
+    else:
+        directions = [(2,0),(-2,0),(0,2),(0,-2)]
+        while directions != []:
+            drow,dcol = random.choice(directions)
+            directions.remove((drow,dcol))
+            row,col = lastPos
+            newRow,newCol = row+drow,col+dcol
+            if nextGridIsValid(app,newRow,newCol,visited):
+                visited.add((newRow,newCol))
+                app.maze[newRow][newCol] = 1
+                app.maze[(row+newRow)//2][(col+newCol)//2] = 1
+                btGenerateMaze(app,visited,(newRow,newCol))
+
+def nextGridIsValid(app,row,col,visited):
+    if (row,col) in visited: return False
+    if not (0 < row < app.row) or not (0 < col < app.col): return False
+    return True
+    
+################################################################################
+
+#Kruskal's Algorithm
+def krusGenerateMaze(app,walls,L = None):
+    #make sure not have aliase when the function is used several times
+    if L is None: 
+        #record all the grids, initially in seperate sets
+        L =[{grid} for grid in sorted(app.grids)]
+    #for the base case, need to check that all grids are in the same set
+    #same as only have one set
+    if len(L) == 1: return
+    else: 
+        (row,col) = random.choice(walls)
+        walls.remove((row,col))
+        if row%2 == 0: 
+            #it means it connected two vertical grids
+            grid1,grid2 = (row-1,col),(row+1,col)
+        else: grid1,grid2 = (row,col-1),(row,col+1)
+
+        for numSet in L:
+            if grid1 in numSet:
+                set1 = numSet
+            if grid2 in numSet:
+                set2 = numSet
+        #If they are in the same set,  just don't do anything
+        if set1 != set2:
+            set3 = set1.union(set2)
+            L.remove(set1)
+            L.remove(set2)
+            L.append(set3)
+            app.maze[row][col] = 1
+
+        krusGenerateMaze(app,walls,L)
+
+################################################################################
+
+#Prim's Algorithm
+def primGenerateMaze(app,grid,visited = None,wallList = None):
+    #make sure not have aliase when the function is used several times
+    if visited is None: 
+        visited = {grid}
+        wallList = primFindWall(app,grid)
+    if wallList == []:
+        return
+    else:
+        row,col = random.choice(wallList)
+        wallList.remove((row,col))
+        if row%2 == 0: 
+            #it means it connected two vertical grids
+            grid1,grid2 = (row-1,col),(row+1,col)
+        else: grid1,grid2 = (row,col-1),(row,col+1)
+
+        newGrid = None
+        if grid1 not in visited:
+            newGrid = grid1
+        if grid2 not in visited:
+            if newGrid == grid1: 
+                primGenerateMaze(app,grid,visited,wallList)
+            newGrid = grid2
+        if newGrid == None:
+            primGenerateMaze(app,grid,visited,wallList)
+        else:
+            visited.add(newGrid)
+            app.maze[row][col] = 1
+            wallList += primFindWall(app,newGrid)
+            primGenerateMaze(app,grid,visited,wallList)
+
+def primFindWall(app,grid):
+    row,col = grid
+    walls = []
+    for drow,dcol in [(1,0),(-1,0),(0,1),(0,-1)]:
+        newRow,newCol = row+drow,col+dcol
+        if (0 < newRow < app.row-1) and (0 < newCol < app.col-1): 
+            walls.append((newRow,newCol))
+    return walls
+
+################################################################################
+
+#Wilson's Algorithm (improved Aldous-Broder)
+#It may takes it too long to find the first way, cause exceeding recursion limit
+def wilsonGenerateMaze(app,visited = None,lastGrid = None):
+    if visited is None:
+        #initialize
+        grid1 = random.choice(sorted(app.grids))
+        visited = {grid1}
+        lastGrid = wilsonChoice(visited,app.grids)
+    lst = wilsonWalk(app,visited,lastGrid,[lastGrid])
+    row,col = lastGrid
+    visited.add(lastGrid)
+    for newRow,newCol in lst[1:]:
+        app.maze[(row+newRow)//2][(col+newCol)//2] = 1 
+        row,col = newRow,newCol
+        visited.add((newRow,newCol))
+    #the base case is actually here
+    if visited == app.grids: return
+    newGrid = wilsonChoice(visited,app.grids)
+    wilsonGenerateMaze(app,visited,newGrid)
+
+def wilsonChoice(visited,grids):
+    #choose grid that are not visited
+    gridlst = sorted(grids.difference(visited))
+    return random.choice(gridlst)
+    
+def wilsonWalk(app,visited,lastGrid,temp):
+    #the random walk part has similar rule as the backtracking method,
+    if lastGrid in visited:
+        return temp
+    directions = [(2,0),(-2,0),(0,2),(0,-2)]
+    while directions != []:
+        drow,dcol = random.choice(directions)
+        directions.remove((drow,dcol))
+        row,col = lastGrid
+        newRow,newCol = row+drow,col+dcol
+        if (0 < row < app.row) and (0 < col < app.col):
+            if (newRow,newCol) in temp:
+                #update the temp, make sure no loop
+                i = temp.index((newRow,newCol))
+                temp = temp[:i]
+            temp.append((newRow,newCol))
+            res = wilsonWalk(app,visited,(newRow,newCol),temp)
+            if res != None: return res
+            temp.pop()
+
+################################################################################
+
+#Hunt-and-Kill algorithm
+def hakGenerateMaze(app,visited = None):
+    if visited is None:
+        #initialize
+        lastGrid = random.choice(sorted(app.grids))
+        visited = set()
+    elif visited == app.grids: return
+    else:
+        #hunting mode
+        lastGrid = hakHunt(app,visited)
+
+    lst = hakWalk(app,visited,lastGrid,[lastGrid])
+    row,col = lastGrid
+    visited.add(lastGrid)
+    for newRow,newCol in lst[1:]:
+        app.maze[(row+newRow)//2][(col+newCol)//2] = 1 
+        row,col = newRow,newCol
+        visited.add((newRow,newCol))
+    hakGenerateMaze(app,visited)
+
+def hakHunt(app,visited):
+    for row in range(1,app.row,2):
+        for col in range(1,app.col,2):
+            if (row,col) not in visited: 
+                for drow,dcol in [(2,0),(-2,0),(0,2),(0,-2)]:
+                    if (row+drow,col+dcol) in visited:
+                        return (row,col)
+
+def hakWalk(app,visited,lastGrid,temp):
+    #the random walk part has similar rule as the backtracking method,
+    if lastGrid in visited:
+        return temp
+    directions = [(2,0),(-2,0),(0,2),(0,-2)]
+    while directions != []:
+        drow,dcol = random.choice(directions)
+        directions.remove((drow,dcol))
+        row,col = lastGrid
+        newRow,newCol = row+drow,col+dcol
+        if nextGridIsValid(app,newRow,newCol,temp):
+            temp.append((newRow,newCol))
+            return hakWalk(app,visited,(newRow,newCol),temp)
+    return temp
+
+################################################################################
+
+#Eller's Algorithm
+def ellerGenerateMaze(app,row = 1,L = None):
+    if L is None:
+        L = [{(1,col)} for col in range(1,app.col,2)]
+    if row == app.row - 2:
+        #base case, join every distinct set
+        col = 1
+        for newCol in range(3,app.col,2):
+            for numSet in L:
+                if (row,col) in numSet:
+                    set1 = numSet
+                if (row,newCol) in numSet:
+                    set2 = numSet
+            if set1 != set2:
+                set3 = set1.union(set2)
+                L.remove(set1)
+                L.remove(set2)
+                L.append(set3)
+                app.maze[row][(col+newCol)//2] = 1
+            col = newCol
+        return
+    else:
+        #horizontal random join
+        col = 1
+        for newCol in range(3,app.col,2):
+            #similar to kruscal
+            for numSet in L:
+                if (row,col) in numSet:
+                    set1 = numSet
+                if (row,newCol) in numSet:
+                    set2 = numSet
+            #If they are in the same set,  just don't do anything
+            if set1 != set2:
+                if random.randint(0,1) == 1: #50% chance
+                    set3 = set1.union(set2)
+                    L.remove(set1)
+                    L.remove(set2)
+                    L.append(set3)
+                    app.maze[row][(col+newCol)//2] = 1
+            col = newCol
+        #vertical random join
+        newRow = row+2
+        for numSet in L:
+            #choice at least one, up to length of set
+            setCopy = copy.copy(numSet)
+            length = len(numSet)
+            for oldRow,oldCol in setCopy:
+                if oldRow == newRow: break
+                index = random.randint(1,length)
+                if index == length:
+                    numSet.add((newRow,oldCol))
+                    app.maze[newRow-1][oldCol] = 1
+                else:
+                    L.append({(newRow,oldCol)})
+                length -= 1
+                numSet.remove((oldRow,oldCol))
+        ellerGenerateMaze(app,newRow,L)
+
+################################################################################
+
+#Recursive Division
+def recDivGenerateMaze(app):
+    #clear all the wall
+    for row,col in app.walls: app.maze[row][col] = 1
+    divideMaze(app,0,0,(app.row+1)//2,(app.col+1)//2)
+
+def divideMaze(app,left,top,rowNum,colNum):
+    if rowNum <= 2 or colNum <= 2:
+        return
+    else:
+        #randomly choose horizontal or vertical devide
+        #index = random.randint(0,1)
+        #When I applied the 50% choice way, it is actually very likely to spawn
+        #many vertical/horizontal long way, so I designed a probably less skewed
+        #and less biased method
+        index = random.randint(int(colNum/rowNum * 100)-100,int(colNum/rowNum * 100))
+        if index <= 50:
+            #horizontal
+            selRow = random.randint(1,rowNum-2)
+            selCol = random.randint(0,colNum-2)
+            rowPos = top+2*selRow
+            for col in range(left+1,left+2*colNum-2):
+                if (col-left-1)//2 != selCol:
+                    app.maze[rowPos][col] = 0
+            divideMaze(app,left,top,selRow+1,colNum)
+            divideMaze(app,left,rowPos,rowNum-selRow,colNum)
+        else:
+            #vertical
+            selRow = random.randint(0,rowNum-2)
+            selCol = random.randint(1,colNum-2)
+            colPos = left+2*selCol
+            for row in range(top+1,top+2*rowNum-2):
+                if (row-top-1)/2 != selRow:
+                    app.maze[row][colPos] = 0
+                else:
+                    app.maze[row][colPos] = 1
+            divideMaze(app,left,top,rowNum,selCol+1)
+            divideMaze(app,colPos,top,rowNum,colNum-selCol)
+
+################################################################################
+
+#Binary Tree
+#This is skewed north west
+def binaryGenerateMaze(app):
+    for row,col in app.grids:
+        pos = []
+        if 0 < row-2 < app.row: pos.append((-1,0))
+        if 0 < col-2 < app.col: pos.append((0,-1))
+        if pos == []: continue
+        drow, dcol = random.choice(pos)
+        app.maze[row+drow][col+dcol] = 1
+
+################################################################################
+
+#Sidewinder Algorithm
+def sideGenerateMaze(app):
+    for row in range(1,app.row,2):
+        if row == 1:
+            for col in range(2,app.col-1,2):
+                app.maze[row][col] = 1
+        else:
+            gridList = []
+            for col in range(1,app.col,2):
+                index = random.randint(0,1)
+                gridList.append((row,col))
+                if index == 1 and 0 < col + 2 < app.col:
+                    app.maze[row][col+1] = 1
+                else:
+                    gRow,gCol = random.choice(gridList)
+                    app.maze[gRow-1][gCol] = 1
+                    gridList = []
+
+def onKeyPress(app,event):
+    if event.isdigit()== True or event == 'r':
+        app.title = 'Initialized Maze'
+        initMaze(app)
+    if event == '1':
+        app.title = 'Recursive Backtracker'
+        btGenerateMaze(app,{(1,1)},(1,1))
+    elif event == '2':
+        app.title = "Kruskal's Algorithm"
+        krusGenerateMaze(app,sorted(app.walls))
+    elif event == '3':
+        app.title = "Prim's Algorithm"
+        primGenerateMaze(app,(1,1))
+    elif event == '4':
+        app.title = "Wilson's Algorithm"
+        wilsonGenerateMaze(app)
+    elif event == '5':
+        app.title = "Hunt-and-Kill Algorithm"
+        hakGenerateMaze(app)
+    elif event == '6':
+        app.title = "Eller's Algorithm"
+        ellerGenerateMaze(app)
+    elif event == '7':
+        app.title = "Recursive Division"
+        recDivGenerateMaze(app)
+    elif event == '8':
+        app.title = "Binary Tree"
+        binaryGenerateMaze(app)
+    elif event == '9':
+        app.title = "Sidewinder Algorith"
+        sideGenerateMaze(app)
+
+def redrawAll(app):
+    drawGrid(app)
+    drawTitle(app)
+
+def drawGrid(app):
+    for row in range(app.row):
+        for col in range(app.col):
+            color = 'black' if app.maze[row][col] == 0 else 'white'
+            para = getCellBound(app,row,col)
+            drawRect(*para,fill = color)
+
+def drawTitle(app):
+    drawLabel(app.title,app.width/2,app.margin/2,size = app.margin/2)
+
+def getCellBound(app,row,col):
+    top = app.margin + col * app.gridSize
+    left = app.margin + row * app.gridSize
+    return top,left,app.gridSize,app.gridSize
+
+runApp()
---
+++ textGraph.py
@@ -0,0 +1,125 @@
+from cmu_cs3_graphics import *
+from cmu_graphics.utils import *
+
+def onAppStart(app):
+    app.width, app.height = 800,800
+    app.mazeCx, app.mazeCy = app.width/2,app.height/2
+    app.mazeW, app.mazeH , app.mazeEdge= 250,750,5
+    app.margin,app.mazeRotate = 10,0
+    app.bounce, app.fraction = 0.8, 0.9
+    app.ballCx, app.ballCy, app.ballR = app.mazeCx, app.mazeCy, 10
+    app.ballyV, app.ballyAcc = 0, 0.6
+    app.ballxV, app.ballxAcc = 10, 0
+    app.paused = False
+    app.rotateSpeed = 0
+
+    app.tempx,app.tempy = 0,0
+    pass
+
+def onStep(app):
+    if not app.paused:
+        doStep(app)
+
+def doStep(app):
+    app.ballyV += app.ballyAcc
+    app.ballCy += app.ballyV 
+    app.ballxV += app.ballxAcc
+    app.ballCx += app.ballxV
+    checkInEdge(app)
+
+def checkInEdge(app):
+     #check if ball touch edge of rect
+    relAngle = math.radians(angleTo(app.mazeCx, app.mazeCy, app.ballCx, app.ballCy))
+    angle = relAngle - math.radians(app.mazeRotate)
+    dis = distance(app.ballCx,app.ballCy,app.mazeCx,app.mazeCy)
+    relx, rely = abs(dis * math.sin(angle)), abs(dis * math.cos(angle))
+    maxRelX, maxRelY = (app.mazeW/2 - app.mazeEdge - app.ballR),(
+        app.mazeH/2 - app.mazeEdge - app.ballR)
+
+    if relx <= maxRelX and rely <= maxRelY:
+        pass
+    else:
+        #Pull back ball
+        newrelx = min(relx,maxRelX)
+        newrely = min(rely,maxRelY)
+        newDis = math.sqrt(newrelx**2 + newrely**2)
+        if 0 < angle%math.pi < math.pi/2:
+            newRelAngle = relAngle - (math.atan(relx/rely)-math.atan(newrelx/newrely))
+        else:
+            newRelAngle = relAngle + (math.atan(relx/rely)-math.atan(newrelx/newrely))
+        app.ballCx = app.mazeCx + newDis * math.sin(newRelAngle)
+        app.ballCy = app.mazeCy - newDis * math.cos(newRelAngle)
+
+        surfaceDeterAngle = math.atan(maxRelX/maxRelY)
+        angle = angle % (2*math.pi)
+        if surfaceDeterAngle < angle <= math.pi - surfaceDeterAngle:
+            bounceAngle = app.mazeRotate%180
+            hitAngle = app.mazeRotate%360
+            hitDirect = ((angle - surfaceDeterAngle)/(math.pi - 2*surfaceDeterAngle)-0.5)*2
+        elif math.pi - surfaceDeterAngle < angle <= math.pi + surfaceDeterAngle:
+            bounceAngle = (90 + app.mazeRotate)%180
+            hitAngle = app.mazeRotate%360 + 90
+            hitDirect = ((angle - (math.pi - surfaceDeterAngle))/(2*surfaceDeterAngle)-0.5)*2
+        elif math.pi + surfaceDeterAngle < angle <= 2* math.pi - surfaceDeterAngle:
+            bounceAngle = app.mazeRotate%180
+            hitAngle = app.mazeRotate%360 + 180
+            hitDirect = ((angle - (math.pi + surfaceDeterAngle))/(math.pi- 2*surfaceDeterAngle)-0.5)*2
+        else:
+            bounceAngle = (90 + app.mazeRotate)%180
+            hitAngle = app.mazeRotate%360 + 270
+            if angle < math.pi:
+                hitDirect = (angle/(2*surfaceDeterAngle)-0.5)*2
+            else:
+                hitDirect = ((angle - (2*math.pi - surfaceDeterAngle))/(math.pi)-0.5)*2
+        bounceAngle = math.radians(bounceAngle)
+        hitAngle = math.radians(hitAngle)
+
+        #Thanks my friend Ken for  teaching me math XD
+        oldxV,oldyV = app.ballxV, app.ballyV
+        app.ballxV = -(math.cos(2*bounceAngle)*oldxV + math.sin(2*bounceAngle)*oldyV)
+        app.ballyV = -(math.sin(2*bounceAngle)*oldxV - math.cos(2*bounceAngle)*oldyV)
+
+        #Add the speed of the box
+        boxSpeed = dis * math.radians(app.rotateSpeed)
+        app.ballxV += boxSpeed * hitDirect * math.cos(hitAngle)
+        app.ballyV += boxSpeed * hitDirect * math.sin(hitAngle)
+        
+        app.ballxV *= app.bounce
+        app.ballyV *= app.bounce
+
+
+def onMousePress(app,eventX,eventY):
+    app.ballxV,app.ballyV = 0,0
+    app.ballCx, app.ballCy = eventX,eventY
+    pass
+
+def onKeyPress(app,key):
+    if key == 'p':
+        app.paused = not app.paused
+    if key == 's':
+        doStep(app)
+
+def onKeyHold(app,key):
+    if 'a' in key:
+        app.mazeRotate -= 1
+        app.rotateSpeed = 1
+    elif 'd' in key:
+        app.mazeRotate += 1
+        app.rotateSpeed = -1
+
+def onKeyRelease(app,key):
+    app.rotateSpeed = 0
+
+def redrawAll(app):
+    # drawCircle(200,200,200,fill = 'white',border = 'black',borderWidth = 5)
+    drawRect(app.mazeCx, app.mazeCy,app.mazeW,app.mazeH,fill = 'white',
+        border = 'black',borderWidth = app.mazeEdge,
+        rotateAngle = app.mazeRotate,align = 'center')
+    drawCircle(app.ballCx, app.ballCy, app.ballR, fill = 'blue')
+    try:
+        drawLine(app.ballCx,app.ballCy,app.ballCx+app.tempx,app.ballCy+app.tempy)
+    except:
+        pass
+    pass
+
+runApp()
